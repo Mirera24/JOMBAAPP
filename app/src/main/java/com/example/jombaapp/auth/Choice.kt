57bb53f0f2ceb629028f297ac3f875408a057635
jeff <geoffreyerastus956@gmail.com>
2024-03-28 17:59:24 -0400

    Initial commit

---
+++ app/src/main/java/com/example/jombaapp/auth/Choice.kt
@@ -0,0 +1,12 @@
+package com.example.jombaapp.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jombaapp.R
+
+class Choice : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choice)
+    }
+}
---
+++ app/src/main/java/com/example/jombaapp/customers/CustomerHome.kt
@@ -0,0 +1,12 @@
+package com.example.jombaapp.customers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jombaapp.R
+
+class CustomerHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_home)
+    }
+}
