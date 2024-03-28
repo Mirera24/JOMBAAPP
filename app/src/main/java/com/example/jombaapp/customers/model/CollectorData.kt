9212671feaae89eceeff71171f7425123aa0bc44
jeff <geoffreyerastus956@gmail.com>
2024-03-28 19:38:28 -0400

    major UI

---
+++ app/src/main/java/com/example/jombaapp/customers/model/CollectorData.kt
@@ -0,0 +1,7 @@
+package com.example.jombaapp.customers.model
+
+data class CollectorData (
+    val uid : String = "",
+    val collectorName : String? = "",
+    val collectorLocation : String? = ""
+)
---
+++ app/src/main/java/com/example/jombaapp/customers/model/MyCollectionsData.kt
@@ -0,0 +1,9 @@
+package com.example.jombaapp.customers.model
+
+data class MyCollectionsData(
+    val uid: String = "",
+    val collectorName : String? = "",
+    val collectorLocation : String? = "",
+    val collectionTime : String? = "",
+    val collectionDate : String? = ""
+)
